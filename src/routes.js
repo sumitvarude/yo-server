17565c4bccf7739c77724d4304c17a75712b1293
Sumit Varude <sumitvar@sumitvar.local>
2017-09-19 14:38:48 +0530

    #08 [Sumit] : Add default route

--- src/routes.js
+++ src/routes.js
@@ -10,4 +10,11 @@ dispatcher.onPost("/sendMessage", function (request, response) {
   response.end("true");
 });
 
+dispatcher.onPost("/", function (request, response) {
+  var messageToSend = request.body;
+  console.log("Message to send------------ =", messageToSend);
+  response.writeHead(200,{'Content-type':'text/plain'});
+  response.end("true");
+});
+
 module.exports.dispatcher = dispatcher;
